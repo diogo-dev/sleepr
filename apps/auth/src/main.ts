0b3a352f8244aff6201a3e9f21194c341bcbfe41
Diogo Felipe <diogofelipe.dev@gmail.com>
2026-03-19 14:52:32 -0300

    feat: authentication (local/jwt strategy)

---
+++ apps/auth/src/main.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { AuthModule } from './auth.module';
+import { Logger } from 'nestjs-pino/Logger';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import cookieParser from 'cookie-parser';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AuthModule);
+
+  app.use(cookieParser());
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,  
+  }));
+  app.useLogger(app.get(Logger));
+
+  const configService = app.get(ConfigService);
+  
+  await app.listen(configService.get('PORT') || 3001);
+}
+bootstrap();
