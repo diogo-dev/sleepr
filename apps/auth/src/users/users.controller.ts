34597438dde8c2b8bd3cc4b3ff167d3e7580be37
Diogo Felipe <diogofelipe.dev@gmail.com>
2026-04-08 11:33:15 -0300

    feat: update user.controller entry point & add ingress.yaml

--- apps/auth/src/users/users.controller.ts
+++ apps/auth/src/users/users.controller.ts
@@ -4,7 +4,7 @@ import { UsersService } from './users.service';
 import { CurrentUser, UserDocument } from '@app/common';
 import { JwtAuthGuard } from '../guards/jwt-auth.guard';
 
-@Controller('users')
+@Controller('auth/users')
 export class UsersController {
 
   constructor(private readonly userService: UsersService) {}
